e57d76ab115edd8e8b2a21f6975eb4dd5654e55c
leedawnn <2g_hee@naver.com>
2022-03-29 17:45:58 +0900

    feat: Make clock

---
+++ src/clock.js
@@ -0,0 +1,12 @@
+function getClock() {
+  const clock = document.querySelector('#clock');
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
