5452687645ae02fee632fa09bf9882a3b9d3635d
leedawnn <2g_hee@naver.com>
2022-03-29 16:31:46 +0900

    feat: Make login feature with localStorage
    1. when localStorage is null, hide greeting phrase.
    2. when localStorage is not null, hide login form.

--- src/app.js
+++ src/app.js
@@ -4,13 +4,26 @@ const inputPw = document.querySelector('.login-form .password');
 const greeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASS = 'hidden';
+const USERNAME_KEY = 'username';
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASS);
   const username = inputId.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(username) {
   greeting.innerText = `Hello, ${username}`;
   greeting.classList.remove(HIDDEN_CLASS);
 }
 
-loginForm.addEventListener('submit', onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASS);
+  loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
